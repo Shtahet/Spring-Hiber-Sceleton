82787151efaf4fcad204491e9ca2ef215786f786
Alexandr Gergalo <derien001@gmal.com>
2018-04-22 11:30:03 +0300

    Create test for SubjectController

---
+++ SundayTimeTable/src/test/java/org/itstep/controller/SubjectControllerTest.java
@@ -0,0 +1,76 @@
+package org.itstep.controller;
+
+import static org.junit.Assert.*;
+
+import java.net.URI;
+
+import org.itstep.ApplicationRunner;
+import org.itstep.model.Subject;
+import org.itstep.service.SubjectService;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ApplicationRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class SubjectControllerTest {
+	
+	private static Subject subject;
+	
+	@Autowired
+	TestRestTemplate restTemplate;
+	
+	@MockBean
+	SubjectService subjectService;
+
+	@BeforeClass
+	public static void setUp() throws Exception {
+		
+		subject = new Subject();
+		subject.setName("Java");
+	}
+
+
+	@Test
+	public void testSave() throws Exception {
+		
+		Mockito.when(subjectService.save(Mockito.any(Subject.class))).thenReturn(subject);
+		
+		RequestEntity<Subject> request = new RequestEntity<Subject>(subject, HttpMethod.POST, new URI("/Subject"));
+		ResponseEntity<Subject> response = restTemplate.exchange(request, Subject.class);
+		
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		Mockito.verify(subjectService, Mockito.times(1)).save(Mockito.any(Subject.class));
+		
+		assertEquals("Java", response.getBody().getName());
+	}
+
+	@Test
+	public void testUpdate() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetOne() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDelete() {
+		//fail("Not yet implemented");
+	}
+
+}
